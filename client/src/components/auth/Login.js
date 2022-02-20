c69979750afc558b267a10619dd728b8bc2567e9
Sebas Quesada <58613474+MrDavidson23@users.noreply.github.com>
2022-02-20 16:53:05 -0600

    EMAIL to email

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,18 +1,15 @@
-import React, { useState, useContext } from 'react';
-import { Link } from 'react-router-dom';
-import { Navigate } from 'react-router-dom';
-import axios from 'axios';
-import Swal from 'sweetalert2';
-import AuthState from '../../context/auth/AuthState';
+import React, { useState, useContext } from "react";
+import { Link } from "react-router-dom";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
+import Swal from "sweetalert2";
+import AuthState from "../../context/auth/AuthState";
 
 const Login = () => {
-
   const [user, setUser] = useState({
-    email: '',
-    password: '',
+    email: "",
+    password: "",
   });
-  
-  
 
   const { email, password } = user;
 
@@ -22,32 +19,32 @@ const Login = () => {
     e.preventDefault();
 
     axios
-      .post('/api/auth', user)
+      .post("/api/auth", user)
       .then((res) => {
         console.log(res);
         Swal.fire({
-          icon: 'success',
-          title: 'Correo y contraseña correctas.',
+          icon: "success",
+          title: "Correo y contraseña correctas.",
           showConfirmButton: false,
           timer: 1500,
         }).then(async () => {
-            try{
-                window.open('/todo', '_self');
-                localStorage.setItem('token', res.data.token);
-            } catch(err){
-                Swal.fire({
-                    icon: 'error',
-                    title: 'ERROR',
-                    showConfirmButton: false,
-                    timer: 1500,
-                  });
-            }
+          try {
+            window.open("/todo", "_self");
+            localStorage.setItem("token", res.data.token);
+          } catch (err) {
+            Swal.fire({
+              icon: "error",
+              title: "ERROR",
+              showConfirmButton: false,
+              timer: 1500,
+            });
+          }
         });
       })
       .catch((err) => {
         Swal.fire({
-          icon: 'error',
-          title: 'Credenciales incorrectos',
+          icon: "error",
+          title: "Credenciales incorrectos",
           showConfirmButton: false,
           timer: 1500,
         });
@@ -55,62 +52,61 @@ const Login = () => {
   };
   return (
     <>
-      <div class='container'>
+      <div class="container">
         <form onSubmit={onSubmit}>
-          <div className='form-group' style={{ textAlign: 'left' }}>
-            <label className='blue-label' style={{ marginLeft: '40px' }}>
-              EMAIL
+          <div className="form-group" style={{ textAlign: "left" }}>
+            <label className="blue-label" style={{ marginLeft: "40px" }}>
+              Email
             </label>
-            <div class='input-group mb-3'>
-              <div class='input-group-prepend' style={{ marginRight: '10px' }}>
-                <span class='input-group-text' id='basic-addon1'>
-                  <i class='fas fa-user'></i>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend" style={{ marginRight: "10px" }}>
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fas fa-user"></i>
                 </span>
               </div>
               <input
-                type='email'
-                name='email'
+                type="email"
+                name="email"
                 value={email}
-                className='form-control'
+                className="form-control"
                 onChange={onChange}
                 required
               />
             </div>
           </div>
 
-          <div className='form-group mb-4' style={{ textAlign: 'left' }}>
-            <label className='blue-label' style={{ marginLeft: '40px' }}>
+          <div className="form-group mb-4" style={{ textAlign: "left" }}>
+            <label className="blue-label" style={{ marginLeft: "40px" }}>
               PASSWORD
             </label>
-            <div class='input-group mb-3'>
-              <div class='input-group-prepend' style={{ marginRight: '10px' }}>
-                <span class='input-group-text' id='basic-addon1'>
-                  <i class='fas fa-lock'></i>
+            <div class="input-group mb-3">
+              <div class="input-group-prepend" style={{ marginRight: "10px" }}>
+                <span class="input-group-text" id="basic-addon1">
+                  <i class="fas fa-lock"></i>
                 </span>
               </div>
               <input
-                type='password'
-                name='password'
+                type="password"
+                name="password"
                 value={password}
-                className='form-control'
+                className="form-control"
                 onChange={onChange}
                 required
               />
             </div>
           </div>
 
-          <div class='container'>
-          <button
-              className='defaultButton2 mb-3'
-              type='submit'
+          <div class="container">
+            <button
+              className="defaultButton2 mb-3"
+              type="submit"
               onClick={onSubmit}
             >
-              {' '}
-                Log In
+              {" "}
+              Log In
             </button>
 
-
-            <Link to='/register' className='defaultButton2 mb-3' type='submit'>
+            <Link to="/register" className="defaultButton2 mb-3" type="submit">
               Register
             </Link>
           </div>
